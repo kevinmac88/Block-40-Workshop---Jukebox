e10ac0b2b19a6f55e01696bf4b4de697b324a1e6
Kevin MacDonald <iamkevinmac@gmail.com>
2025-10-30 13:23:34 -0700

    started sql

---
+++ db/schema.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS playlist_tracks;
+DROP TABLE IF EXISTS tracks;
+DROP TABLE IF EXISTS playlists;
+
+CREATE TABLE playlists (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE tracks (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    duration_ms INTEGER NOT NULL
+);
+
+-- this is our junciton table, customary to just combine the 2 tables it refernces in the name
+CREATE TABLE playlists_tracks (
+    id SERIAL PRIMARY KEY,
+    playlist_id INTEGER NOT NULL,
+    track_id INTEGER NOT NULL
+
+    CONSTRAINT fk_playlist
+    FOREIGN KEY (playlist_id)
+    REFERENCES playlists(id)
+    ON DELETE CASCADE,
+
+    CONSTRAINT fk_track
+    FOREIGN KEY (track_id)
+    REFERENCES tracks(id)
+    ON DELETE CASCADE,
+
+-- this one ensures a track can only be in a playlist one time
+    CONSTRAINT unique_playlist_track
+    UNIQUE (playlist_id, track_id)
+)
